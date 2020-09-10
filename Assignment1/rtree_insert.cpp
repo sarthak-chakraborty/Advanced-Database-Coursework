15cf646ea5d6d9f9cf30c5ad17ca0a33e8b8efb1
omareqbal <eqbalomar@gmail.com>
2020-09-10 18:54:11 +0530

    write tree to file

--- Assignment1/rtree_insert.cpp
+++ Assignment1/rtree_insert.cpp
@@ -8,6 +8,9 @@ using namespace std;
 /* Global Variables for Max and Min number of children */
 int M=-1, m=-1;
 
+/* Counts for Nodes and Rectangles */
+int node_count = 0, rect_count = 0;
+
 
 /* Choose Leaf Algorithm */
 RTNode* ChooseLeaf(RTNode* node, Rect* E){
@@ -52,6 +55,70 @@ RTNode* ChooseLeaf(RTNode* node, Rect* E){
 }
 
 
+/* Write tree to a file 
+
+Each line corresponds to a node
+
+node_num  parent_rect_num  R1(rect_num, dmin[], dmax[], child_node_num)  R2(dmin[], dmax[], child_node_num)
+
+*/
+void WriteTree(RTNode* node, FILE* fout){
+    
+    if(node == NULL){
+        return;
+    }
+    
+    /* Write node_num and parent_rect_num */
+
+    if(node->parent == NULL){
+        node->RTNode_num = node_count++;
+    }
+
+    fprintf(fout, "%d ", node->RTNode_num);
+
+    if(node->parent != NULL){
+        fprintf(fout, "%d ", node->parent->Rect_num);
+    }
+    else{
+        fprintf(fout, "-1 ");
+    }
+
+    /* Write rectangles in the node */
+    int dim = (node->entry[0]).dmin.size(), num_entry = (node->entry).size();
+
+    Rect* cur_entry;
+    for(int i = 0; i < num_entry; i++){
+        cur_entry = &(node->entry[i]);
+
+        /* Write rect_num and rect coodinates */
+        cur_entry->Rect_num = rect_count++;        
+        fprintf(fout, "%d ", cur_entry->Rect_num);
+        
+        for(int j = 0; j < dim; j++){
+            fprintf(fout, "%d %d ", cur_entry->dmin[j], cur_entry->dmax[j]);
+        }
+        
+        /* Write child_node_num */
+        int child_node_num = -1;
+        if(cur_entry->child != NULL){
+            child_node_num = node_count++;
+            cur_entry->child->RTNode_num = child_node_num;
+        }
+        fprintf(fout, "%d ", child_node_num);
+    }
+
+    fprintf(fout, "\n");
+    
+    /* Write subtrees */
+    for(int i = 0; i < num_entry; i++){
+        cur_entry = &(node->entry[i]);
+        if(cur_entry->child != NULL){
+            WriteTree(cur_entry->child, fout);
+        }
+    }
+}
+
+
 int main(int argc, char** argv){
     int n = -1;
     string filename;
@@ -73,11 +140,19 @@ int main(int argc, char** argv){
     }
 
     /* Create filename */
-    filename = "testdata_dim=" + to_string(n) + ".txt";
+    filename = "rtree_dim=" + to_string(n) + ".txt";
 
-    /* Initialize M and m (Max and Min number of childrens for a node) */
+    /* Initialize M and m (Max and Min number of children for a node) */
     M = floor(4096 / (4*n+1));
     m = floor(M/2);
+    
+    /*
+    FILE* fout = fopen(filename.c_str(), "w");
+
+    WriteTree(root, filename);
+
+    fclose(fout);
+    */
 
     return 0;
 }

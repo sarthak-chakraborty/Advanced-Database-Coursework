cea5c3dc1a35c29d63918a7bde1658df5bf423f8
omareqbal <eqbalomar@gmail.com>
2020-09-30 09:32:30 +0530

    normalize for query image

--- Assignment3/queryImgToHist.py
+++ Assignment3/queryImgToHist.py
@@ -8,6 +8,17 @@ from PIL import Image, ImageOps
 import sys
 import os
 
+
+def normalize(arr):
+    arr = arr.astype('float')
+    minval = arr.min()
+    maxval = arr.max()
+    if minval != maxval:
+        arr -= minval
+        arr *= (255.0/(maxval-minval))
+    return arr
+
+
 if(len(sys.argv) < 2):
     sys.exit('Image file name required')
 
@@ -16,7 +27,12 @@ image = sys.argv[1]
 img = Image.open(os.path.join(os.getcwd(), image))
 img_grayscale = ImageOps.grayscale(img)
 
-hist = img_grayscale.histogram()
+arr = np.array(img_grayscale)
+img2 = Image.fromarray(normalize(arr).astype('uint8'))
+img3 = ImageOps.equalize(img2, mask=None)
+
+hist = img3.histogram()
+
 
 fd_out = open(os.path.join(os.getcwd(), 'query_image.txt'), 'w')
 for i in range(len(hist)):
